bc8e92da9d0e30aa4830395a0e7d0398161d519b
Sebastien Corbisier <70596078+Sbyspaceopera@users.noreply.github.com>
2023-06-11 16:49:21 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,116 @@
+import { useState } from 'react';
+import images from './imagesArray';
+
+function App() {
+  const [selectedImages, setSelectedImages] = useState([]);
+  const [selectedKeywords, setSelectedKeywords] = useState([]);
+  const [isKeywordsVisible, setIsKeywordsVisible] = useState(true);
+
+  const handleImageClick = (image) => {
+    const imageIndex = selectedImages.findIndex((img) => img.id === image.id);
+    if (imageIndex === -1) {
+      setSelectedImages((prevImages) => [...prevImages, image]);
+      setSelectedKeywords((prevKeywords) => [
+        ...prevKeywords,
+        ...image.keywords,
+      ]);
+    } else {
+      setSelectedImages((prevImages) =>
+        prevImages.filter((img) => img.id !== image.id)
+      );
+      setSelectedKeywords((prevKeywords) =>
+        prevKeywords.filter(
+          (keyword) => !image.keywords.some((kw) => kw.name === keyword.name)
+        )
+      );
+    }
+  };
+
+  const handleTitleClick = (image) => {
+    const imageIndex = selectedImages.findIndex((img) => img.id === image.id);
+    if (imageIndex === -1) {
+      setSelectedImages((prevImages) => [...prevImages, image]);
+      setSelectedKeywords((prevKeywords) => [
+        ...prevKeywords,
+        ...image.keywords,
+      ]);
+    } else {
+      setSelectedImages((prevImages) =>
+        prevImages.filter((img) => img.id !== image.id)
+      );
+      setSelectedKeywords((prevKeywords) =>
+        prevKeywords.filter(
+          (keyword) => !image.keywords.some((kw) => kw.name === keyword.name)
+        )
+      );
+    }
+  };
+
+  const handleKeywordsVisibility = () => {
+    setIsKeywordsVisible(!isKeywordsVisible);
+  };
+
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100 px-4">
+      <h1 className="text-4xl font-bold mb-8">Choisissez ce que vous aimez</h1>
+
+      {/*BUTTON */}
+      <button
+        onClick={handleKeywordsVisibility}
+        className="mb-4 px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none"
+      >
+        {isKeywordsVisible ? 'Cacher les mots-clés' : 'Voir les mots-clés'}
+      </button>
+
+      {/*KEYWORDS */}
+      {isKeywordsVisible && (
+        <div className="flex flex-wrap justify-center">
+          {selectedKeywords.map((keyword) => (
+            <div
+              key={keyword.id}
+              className="keyword m-2 px-3 py-1 bg-green-500 text-white rounded-full"
+            >
+              {keyword.name}
+            </div>
+          ))}
+        </div>
+      )}
+
+      {/*IMAGES */}
+      <div className="relative grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 my-8">
+        {images.map((image) => (
+          <div key={image.id} className="relative">
+            <img
+              src={image.url}
+              alt={image.name}
+              className={`h-48 object-cover aspect-video cursor-pointer transition duration-500 ease-in-out transform ${
+                selectedImages.some(
+                  (selectedImage) => selectedImage.id === image.id
+                )
+                  ? 'scale-110'
+                  : ''
+              }`}
+              onClick={() => handleImageClick(image)}
+            />
+            <div
+              className={`absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 text-white flex items-center justify-center px-4 cursor-pointer transition duration-500 ease-in-out transform ${
+                selectedImages.some(
+                  (selectedImage) => selectedImage.id === image.id
+                )
+                  ? 'scale-110'
+                  : ''
+              }`}
+              onClick={() => handleTitleClick(image)}
+            >
+              {image.title}
+            </div>
+          </div>
+        ))}
+      </div>
+
+      
+    </main>
+  );
+}
+
+export default App;
---
+++ src/imagesArray.js
@@ -0,0 +1,189 @@
+const images = [
+    {
+      id: 1,
+      name: "Sport",
+      title: "Sport",
+      url: "https://via.placeholder.com/150/92c952",
+      keywords: [
+        { id: 1, name: "Sport" },
+      ]
+    },
+    {
+      id: 2,
+      name: "Musique",
+      title: "Musique",
+      url: "https://via.placeholder.com/150/771796",
+      keywords: [{ id: 3, name: "Musique" }]
+    },
+    {
+      id: 3,
+      name: "Humour",
+      title: "Humour",
+      url: "https://via.placeholder.com/150/24f355",
+      keywords: [
+        { id: 4, name: "Humour" },
+      ]
+    },
+    {
+      id: 4,
+      name: "Santé",
+      title: "Santé",
+      url: "https://via.placeholder.com/150/24f355",
+      keywords: [
+        { id: 6, name: "Santé" }
+      ]
+    },
+    {
+      id: 5,
+      name: "Tutoriels",
+      title: "Tutoriels",
+      url: "https://via.placeholder.com/150/24f355",
+      keywords: [
+        { id: 8, name: "Tuto" }
+      ]
+    },
+    {
+      id: 6,
+      name: "Vlogs",
+      title: "Vlogs",
+      url: "https://via.placeholder.com/150/24f355",
+      keywords: [
+        { id: 10, name: "Vlog" }
+      ]
+    },
+    {
+      id: 7,
+      name: "Jeux Vidéos",
+      title: "Jeux Vidéos",
+      url: "https://via.placeholder.com/150/24f355",
+      keywords: [
+        { id: 12, name: "Gaming" }
+      ]
+    },
+    {
+      id: 8,
+      name: "Education",
+      title: "Education",
+      url: "https://via.placeholder.com/150/24f355",
+      keywords: [
+        { id: 14, name: "Education" },
+      ]
+    },
+    {
+      id: 9,
+      name: "Sciences",
+      title: "Sciences",
+      url: "https://via.placeholder.com/150/24f355",
+      keywords: [
+        { id: 16, name: "Sciences" },
+      ]
+    },
+    {
+      id: 10,
+      name: "Beauté",
+      title: "Beauté",
+      url: "https://via.placeholder.com/150/24f355",
+      keywords: [
+        { id: 18, name: "Beauté" },
+        { id: 19, name: "Makup" }
+      ]
+    },
+    {
+      id: 11,
+      name: "Animaux",
+      title: "Animaux",
+      url: "https://via.placeholder.com/150/24f355",
+      keywords: [
+        { id: 20, name: "Animaux" }
+      ]
+    },
+    {
+      id: 12,
+      name: "Documentaires",
+      title: "Documentaires",
+      url: "https://via.placeholder.com/150/24f355",
+      keywords: [
+        { id: 22, name: "Documentaire" },
+      ]
+    },
+    {
+      id: 13,
+      name: "Cinéma/Séries",
+      title: "Cinéma/Séries",
+      url: "https://via.placeholder.com/150/24f355",
+      keywords: [
+        { id: 24, name: "Cinéma" },
+        { id: 25, name: "Séries" }
+      ]
+    },
+    {
+      id: 14,
+      name: "Actualités",
+      title: "Actualités",
+      url: "https://via.placeholder.com/150/24f355",
+      keywords: [
+        { id: 26, name: "Actualités" },
+      ]
+    },
+    {
+      id: 15,
+      name: "Technologies",
+      title: "Technologies",
+      url: "https://via.placeholder.com/150/24f355",
+      keywords: [
+        { id: 28, name: "Technologies" },
+      ]
+    },
+    {
+      id: 16,
+      name: "Image 16",
+      title: "Title 16",
+      url: "https://via.placeholder.com/150/24f355",
+      keywords: [
+        { id: 30, name: "Keyword 30" },
+        { id: 31, name: "Keyword 31" }
+      ]
+    },
+    {
+      id: 17,
+      name: "Image 17",
+      title: "Title 17",
+      url: "https://via.placeholder.com/150/24f355",
+      keywords: [
+        { id: 32, name: "Keyword 32" },
+        { id: 33, name: "Keyword 33" }
+      ]
+    },
+    {
+      id: 18,
+      name: "Image 18",
+      title: "Title 18",
+      url: "https://via.placeholder.com/150/24f355",
+      keywords: [
+        { id: 34, name: "Keyword 34" },
+        { id: 35, name: "Keyword 35" }
+      ]
+    },
+    {
+      id: 19,
+      name: "Image 19",
+      title: "Title 19",
+      url: "https://via.placeholder.com/150/24f355",
+      keywords: [
+        { id: 36, name: "Keyword 36" },
+        { id: 37, name: "Keyword 37" }
+      ]
+    },
+    {
+      id: 20,
+      name: "Image 20",
+      title: "Title 20",
+      url: "https://via.placeholder.com/150/24f355",
+      keywords: [
+        { id: 38, name: "Keyword 38" },
+        { id: 39, name: "Keyword 39" }
+      ]
+    }
+  ];
+
+  export default images;
